e7d9215a46c5e7eb7b7fec16828185028c4179a6
hahamimidido <eason@tpe.hyweb.com.tw>
2020-06-05 15:47:08 +0800

    add 多媒體拖曳區域

--- js/customize.js
+++ js/customize.js
@@ -312,4 +312,68 @@ $(function() {
             e.preventDefault();
         });
     });
+    // 首頁數據
+    $(document).ready(function() {
+        $('[data-num]').each(function() {
+            startLoop($(this));
+        });
+
+        function startLoop($it) {
+            var cur = 0;
+            var num = parseInt($it.attr('data-num'));
+            init($it, num, cur);
+
+            function init($it, num, cur) {
+                var steps = 100;
+                var timetotal = 3000;
+                var cur = 0;
+                var steptimeout = timetotal / steps;
+                var numlen = (num + '').length;
+                $it.html(getHtm(0, numlen));
+                setTimeout(loop, 1000);
+
+                function setValue(val) {
+                    $it.html(getHtm(val, numlen));
+                }
+
+                function getFormated(n, lentotal) {
+                    var t = ",",
+                        i = n + "";
+                    if (i.length < lentotal) {
+                        var y = lentotal - i.length;
+                        for (var z = 0; z < y; z++) {
+                            i = '0' + i;
+                        }
+                    }
+                    var j = (j = i.length) > 3 ? j % 3 : 0;
+                    return (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t);
+                }
+
+                function loop() {
+                    cur++
+                    var percent = cur / steps;
+                    if (percent >= 1) {
+                        setValue(parseInt(num));
+                    } else {
+                        setValue(parseInt(percent * num));
+                        setTimeout(loop, steptimeout);
+                    }
+                }
+
+                function getHtm(num, lentotal) {
+                    var targetFormated = getFormated(num, lentotal);
+                    var chars = targetFormated.split('');
+                    var htms = [];
+                    for (var i = 0; i < chars.length; i++) {
+                        if (chars[i] == ',') {
+                            htms += '';
+                        } else {
+                            htms += '<span>' + chars[i] + '</span>';
+                        }
+                    }
+                    return htms;
+                }
+            }
+        }
+    });
 });
